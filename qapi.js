0b100e00e23215f3e20efab01d17d8d175805951
sosoosjsjnab <74925201+sosoosjsjnab@users.noreply.github.com>
2020-11-24 22:43:14 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,48 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbd5f5eb1c8af001eb5743a",
+				"5fbd5f5eb1c8af001eb5743c",
+				"5fbd619bb1c8af001eb574ad",
+				"5fbd61cab1c8af001eb574b7",
+				"5fbd61e708b41a001fec72c7"
+			]
+		},
+		"5fbd5f5eb1c8af001eb5743a": {
+			"id": "5fbd5f5eb1c8af001eb5743a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbd5f5eb1c8af001eb5743c": {
+			"id": "5fbd5f5eb1c8af001eb5743c",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/man-in-a-blue-shirt-does-window-installation-860487134-5c946c7346e0fb0001d880b7.jpg?v=2020-11-24T19:39:22.540Z",
+				"title": "Building tips",
+				"description": "How to make slopes with your own hands?"
+			}
+		},
+		"5fbd619bb1c8af001eb574ad": {
+			"id": "5fbd619bb1c8af001eb574ad",
+			"pageUrl": "terms",
+			"name": "Terms And Conditions"
+		},
+		"5fbd61cab1c8af001eb574b7": {
+			"id": "5fbd61cab1c8af001eb574b7",
+			"pageUrl": "privacy",
+			"name": "Privacy Policy"
+		},
+		"5fbd61e708b41a001fec72c7": {
+			"id": "5fbd61e708b41a001fec72c7",
+			"pageUrl": "cookies",
+			"name": "Cookies"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,199 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Image, Box, Icon } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
+import { MdFavorite } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Building tips
+			</title>
+			<meta name={"description"} content={"How to make slopes with your own hands?"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/man-in-a-blue-shirt-does-window-installation-860487134-5c946c7346e0fb0001d880b7.jpg?v=2020-11-24T19:39:22.540Z"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/man-in-a-blue-shirt-does-window-installation-860487134-5c946c7346e0fb0001d880b7.jpg?v=2020-11-24T19:39:22.540Z) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						How to make slopes with your own hands?
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" color="--dark">
+			<Stack>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+						After installing windows or doors, it is necessary to make the slopes correctly. They significantly improve the appearance of the structure, prevent heat loss through cracks. Different materials are used to carry out such a finish. If desired, this work can be done independently. To do this, you need to consider in detail the manufacturing technology of such a finish. How to make slopes with your own hands will be discussed later. Purpose of slopes Before considering how to make slopes with your own hands, you need to consider their purpose. Some owners of houses and apartments believe that it is enough to blow out the joints with polyurethane foam after installing plastic windows or doors. However, this is not the case. Polyurethane foam will not fully protect the home from heat loss. It should be said that such material requires additional protection. If the polyurethane foam is uncovered for a long time, it will actively absorb moisture. Temperature fluctuations also have a devastating effect on it. As a result, the insulating layer is quickly destroyed. He will no longer be able to protect the room from drafts and noise. Heat loss through improperly mounted slopes is up to 40%. Correctly executed slopes can not only protect assembly seams from damage. They will act as additional thermal insulation. Slopes are also a decorative finish. This will prevent the windows from fogging up.
+					</Text>
+				</StackItem>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+						Varieties To understand how to make slopes on doors and windows, you need to consider their varieties in detail. This type of finish can be made from different materials. Previously, the plastering method was used to create slopes. New materials have appeared today. They are easy to install and durable. The most popular type is plastic slopes. They are in harmony with plastic windows or balcony blocks. These materials are similar in their technical characteristics. The plastic is durable and easy to clean. He is not afraid of temperature changes. Special sandwich panels are one of the new types of plastic slopes. They include a layer of insulation. This greatly simplifies the installation procedure. Another popular type of slope is drywall trim. This is one of the most inexpensive materials. It has high environmental performance. Drywall can be used in any room. Features of plastering In order to choose the right finishing technology, it is necessary to consider in detail how to make slopes on plastic windows. The most ancient is the method of plastering. It was used back in the 90s of the last century. Plastering will be an acceptable option if major or complete redecoration is being performed in the room.
+					</Text>
+				</StackItem>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+						In the process of applying the plaster, the walls will get dirty. If they already have a layer of wallpaper or other finishing, it will be damaged. Therefore, this option is not suitable for already renovated premises. This method often involves installing beacons. These corners will help strengthen the base of a window or doorway. When dismantling an old window, building materials in this area can deform, crack and crumble. To prevent further destruction of the surface of the opening, corners are used. Preparation for plastering There is a relatively simple and time-tested method of how to make slopes on windows with your own hands from plaster. First you need to install beacons. The first of them is installed so that the plaster mixture lies on the frame by at least 5 cm. The lighthouse must be in a strictly horizontal position. The second corner must be installed at the corners of the window or doorway. The distance between all beacons must be the same. Skews are unacceptable. This is checked using a building level. If the slopes need to be finished from the inside of the room, you can use a gypsum plaster mixture. It is environmentally friendly, non-flammable. Even large potholes and cracks can be covered with such a composition. If you need to make a layer more than 3 cm, fine sieved sand is added to the composition. For exterior work, a special facade plaster or cement mixture with water-repellent components is used.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						color="--dark"
+						padding="98px 64px"
+						background="--color-light"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="12px 0"
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+					>
+						Plastering procedure Studying the technology, how to make the slopes correctly, you should pay special attention to the plastering process. When the appropriate type of putty has been mixed, you can start working. The composition is applied to the surface in several layers. When the first layer is created, it is allowed to dry. First, spray is done. The first layer can be quite rough. For this, a special starting gypsum putty can be used (for interior work). After it dries, priming is carried out. On the first layer, plaster is laid out a second time. It must be carefully leveled. The third layer will be the finishing one. It is created from a special putty. It consists of a fine fraction. The result is a perfectly flat white surface. The finishing layer is only a few millimeters long. When it dries well, the surface is treated with fine-grain sandpaper. You can also use a sander. Completion of the plastering process After applying the plaster, the correct final stage of this type of finish must be carried out. Studying how to make slopes on windows, this technology needs to be considered in detail. In places where the putty is connected to the door block, you need to cover up the cracks. For this, a special sealant is used. The joint is cut with a clerical knife to a depth of 3 mm. The angle of inclination should be 45º. The recess must be primed and sealed with a sealant. You need to do this procedure carefully. The line should be very straight. This will help the use of masking tape. The sealant layer must be leveled with a rubber trowel. Then, until the sealant dries, the tape is removed. Before painting, the plaster layer is primed. For finishing, you can use oil-based, acrylic-based compounds. The second option is preferable. The paint is applied in 2 layers. This is necessary to create the desired shade. The first layer should dry well. Only then can the paint be applied a second time. The process of filling the slopes takes about a week.
+					</Text>
+					<Text as="h2" margin="12px 0" font="--headline2" md-font="--headline3" />
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Stack>
+						<StackItem width="100%" lg-width="33.3%" md-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/lyuboj-remont-nachinat-stoit-s-zameny-okon-www-ural-org.jpg?v=2020-11-24T19:39:22.520Z) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/Remont_i_zamena_podokonnikov_-_chto_mozhno_sdelat_esli_s_podokonnikom_problemy__6.jpg?v=2020-11-24T19:39:22.518Z) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/otdelka-okonnyh-otkosov-vnutri-pomeshcheniya31-scaled.jpg?v=2020-11-24T19:39:22.517Z) center/cover" />
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Plastic slopes Knowing how to finish using plaster, you need to consider the method of how to make plastic slopes. This is one of the most popular methods today. With temperature changes, the material of metal-plastic windows and slopes will expand and contract simultaneously. Therefore, this type of finish is popular. Plastic is used for decoration of windows both inside and outside the premises. There are three types of slopes in this category. The first of them includes foam plastic panels. The second group includes sandwich panels. The third category includes mixed finishes. The slope in this case consists of plasterboard with a plastic top layer. Sandwich panels are often used when arranging plastic windows and doors. They differ in acceptable cost. Moreover, they are easy to mount. This material does not cause condensation on the windows. Their thickness should be more than 1 cm. You can choose any color and texture of the plastic surface. Slopes will harmoniously fit into the interior. Installation of sandwich panels There is a simple technique for making slopes out of panels. On the outer edge of the opening, you need to fix a wooden strip with self-tapping screws. It should be even, which is checked using a building level. This rail will take the load of the sandwich panel. Next, you need to mount the starting profile in the grooves. You will need to attach a slope to it. Next, it is insulated. A layer of mineral wool or polystyrene is laid behind the plastic. The first option is preferable. It does not burn, it is an environmentally friendly material. From the outside, the slopes are mounted to the wall. This procedure is performed using self-tapping screws. Their hats are covered with special plugs. You can also fix the slopes with brackets. But this is a more laborious and complicated process. The first option is simpler, so it is used much more often.{" "}
+			</Text>
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/360.jpg?v=2020-11-24T19:39:22.511Z" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/kakie-otkosy-vybrat-dlya-okna-1.jpg?v=2020-11-24T19:39:22.512Z" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd5f5eb1c8af001eb57437/images/otdelka-okonnyh-otkosov-vnutri-pomeshcheniya33.jpg?v=2020-11-24T19:39:22.505Z" width="320px" max-width="100%" />
+				</Box>
+			</Box>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--base" margin="20px 0 0 0">
+				Foamed PVC slopes Next, you should consider another option, how to make plastic slopes. In this case, foamed PVC is used. They feature a smooth top layer. Semicircular slopes can be made from this material. In order to bend the PVC foam strip, it is cut in several places. The groove should not exceed one third of the panel thickness. After giving the sheet the required shape, the cuts must be coated with glue. Installation requires preliminary fixing of the profile. The panel will be installed on it. The space between the wall and the PVC sheet must be blown out with foam. This procedure is performed carefully so that the foam does not expand too much inside the structure. After that, the profile is fixed to the outer corner. The joints must be carefully treated with silicone. Their surfaces are smoothed with a rubber spatula. For panels that combine drywall and plastic, installation is carried out in the same way as for sandwich panels. Plasterboard slopes Another option for how to make slopes on the windows is to use drywall. It is a practical, relatively inexpensive material. If its top layer becomes dirty or damaged for some reason, it can be repaired. In this case, the drywall is putty and plaster. The disadvantage of this finishing method is the lengthy installation process. Several steps are required. First, a wireframe is created. You can do without it if the surface of the base is flat. Next, sheets of drywall are laid. They are putty, covered with a primer. This is followed by painting. In favor of the type of finish presented, we can say that this is one of the most durable types of finishes. Plasterboard slopes are easy to clean. This method is only suitable for indoor use and dry rooms. This material is highly absorbent. A special moisture-resistant drywall is on sale. However, its cost will be much higher.
+			</Text>
+		</Section>
+		<Section color="--dark">
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdFavorite}
+				/>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					Frame installation method Consider how to make drywall slopes when using a frame. This requires a galvanized profile. If this procedure is carried out for door slopes, metal guides can be used. First, the opening is blown out with foam. Its surplus is cut off after a day. A guide profile must be installed along the perimeter of the opening. The frame should consist of galvanized guides. When it is assembled, a layer of mineral wool is placed inside the structure. After that, drywall sheets can be mounted on top of the frame. It is polished and putty. The corners are closed with a metal corner profile. This will prevent delamination. When the drywall is ready for further processing, it is covered with a layer of primer. When it's dry, you can apply a coat of acrylic paint. Frameless method You should also consider how to make drywall slopes without a frame. This method will not always work. Only if the old slopes have been previously installed can new trim sheets be mounted on them. The base is cleaned, covered with a primer. Dotted Perlfix glue is applied to drywall. Then it is installed on the base and additionally fixed to the old frame with self-tapping screws. When the materials are dry, they are covered with a finishing putty. Having considered how to make slopes, you can choose the right finishing method and complete the entire procedure yourself.
+				</Text>
+			</Box>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0" background="rgba(0, 0, 0, 0.07)">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Contact us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Emmerweg 46 Dalen, Drenthe(DR), 7751 AM 06-72673955
+			</Text>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0" background="rgba(0, 0, 0, 0.12)">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				<Link href="#">
+					Terms And Conditions
+				</Link>
+				{" "}-{" "}
+				<Link
+					href="#"
+					overflow-wrap="normal"
+					word-break="normal"
+					white-space="normal"
+					text-indent="0"
+					text-overflow="clip"
+					hyphens="manual"
+				>
+					Privacy Policy
+				</Link>
+				{" "}-{" "}
+				<Link
+					href="#"
+					overflow-wrap="normal"
+					word-break="normal"
+					white-space="normal"
+					text-indent="0"
+					text-overflow="clip"
+					hyphens="manual"
+				>
+					Cookies
+				</Link>
+			</Text>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
